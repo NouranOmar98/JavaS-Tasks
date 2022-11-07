97f935e1dd9d152be43f67cbfd6e7d06479047e0
NouranOmar98 <norankinane@gmail.com>
2022-11-07 21:49:46 +0300

    Tasks

---
+++ 7 Nov/task3.js
@@ -0,0 +1,13 @@
+let shortestLength = 100;
+let shortestValue ;
+
+function shorterInArray(arr){
+for(let x=0; x<arr.length; x++){
+if(arr[x].length < shortestLength){
+    shortestLength = arr[x].length;
+    shortestValue = arr[x];
+    }
+    }
+    document.write(shortestValue);
+}
+shorterInArray(["alex", "mercer", "madrasa", "rashed2", "emad", "hala"]);
